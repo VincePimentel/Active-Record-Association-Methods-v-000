002a2233f524346d33408d9e914d1a1601054187
Vince Andrew Pimentel <contact@vincep.me>
2018-12-26 21:47:30 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,6 +16,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
+    self.artists.map do |artist|
+      artist.name
+    end
   end
 end
